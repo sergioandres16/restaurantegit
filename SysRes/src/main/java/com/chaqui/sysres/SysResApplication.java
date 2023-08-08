c69895ee5bdefe78b0c485a2c00dd07b148239f3
Josue Fuentes <josue.chaqui@gmail.com>
2023-08-08 13:18:03 -0600

    agregando codigo

---
+++ SysRes/src/main/java/com/chaqui/sysres/SysResApplication.java
@@ -0,0 +1,13 @@
+package com.chaqui.sysres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysResApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SysResApplication.class, args);
+    }
+
+}
---
+++ SysRes/src/test/java/com/chaqui/sysres/SysResApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chaqui.sysres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SysResApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
